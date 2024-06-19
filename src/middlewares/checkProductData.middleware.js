84f0d08f7bd101429e6c6d7860014d9f84d5b9f9
victoriashopify <victoria@bluekube.com>
2024-06-18 22:23:27 -0300

    commit

--- src/middlewares/checkProductData.middleware.js
+++ src/middlewares/checkProductData.middleware.js
@@ -1,7 +1,6 @@
-
 export const checkProductData = async (req, res, next) => {
   try {
-    const { title, description, price,  code, stock, category } = req.body;
+    const { title, description, price, code, stock, category } = req.body;
     const newProduct = {
       title,
       description,
@@ -11,15 +10,17 @@ export const checkProductData = async (req, res, next) => {
       category,
     };
 
-    // verificar que el producto tenga todas las propiedades
     if (Object.values(newProduct).includes(undefined)) {
-      return res.status(400).json({ status: "error", msg: "Todos los campos son obligatorios" });
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Todos los campos son obligatorios" });
     }
 
-    // Next permite que continué la ejecución del endpoint
     next();
   } catch (error) {
     console.log(error);
-    res.status(500).json({ status: "error", msg: "Error interno del servidor" });
+    res
+      .status(500)
+      .json({ status: "error", msg: "Error interno del servidor" });
   }
 };
